1c6ce5435391cebce23a23ef25a6882161d62bf1
Meet Patel <meetp4499@gmail.com>
2026-03-27 08:23:59 +0530

    chore: untrack build

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/meetpatel12_/Stuff/Code/C++/thread-pool/build/_deps/googletest-src/googletest
-# Build directory: /home/meetpatel12_/Stuff/Code/C++/thread-pool/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
